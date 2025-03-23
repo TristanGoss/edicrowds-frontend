1f3ce28fa58a0ae4d900ea6df7591e01d70fa557
Tristan Goss <tristanjdgoss@gmail.com>
2025-03-23 00:48:00 +0000

    inject buymeacoffee into footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,7 +13,16 @@ export default function Footer() {
         © Edinburgh Crowds 2025
         <Link to="/terms" className={linkClass("/terms")}>Terms of Service</Link>
         <Link to="/privacy" className={linkClass("/privacy")}>Privacy Policy</Link>
+        <a
+          href="https://www.buymeacoffee.com/edinburghcrowds"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="internal-link"
+        >
+          Support The Project
+        </a>
         <a href="mailto:info@edinburghcrowds.co.uk" className="internal-link">info@edinburghcrowds.co.uk</a>
+
       </div>
     </footer>
   );
